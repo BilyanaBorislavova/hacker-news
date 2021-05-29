0937083859b107f1da10d98bb0acf458a6e59105
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-29 13:16:48 +0300

    Added functionality to get 10 random top stories on every render

--- src/components/hacker-news.tsx
+++ src/components/hacker-news.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import useHackerNewsTopStories from '../hooks/use-hacker-news-top-stories';
 import HackerNewsService from '../services/hacker-news-service';
+import { shuffleArray } from '../utils/array-utils';
+
+const MAX_TOP_STORIES_TO_RETRIEVE = 10;
 
 const HackerNews = () => {
     const hackerNewsService = new HackerNewsService();
     const { topStories, loading } = useHackerNewsTopStories(hackerNewsService);
-    console.log(topStories, loading)
+    const tenRandomizedTopStories = !loading && shuffleArray(topStories).slice(0, MAX_TOP_STORIES_TO_RETRIEVE);
+
     return (
         <section>123</section>
     )
