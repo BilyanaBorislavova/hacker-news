9988aa124db443a4cf4c5f650fe3c641493a3ffe
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-30 00:17:03 +0300

    Update hacker-news.tsx

--- src/components/hacker-news.tsx
+++ src/components/hacker-news.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import useFetch, { FetchContext } from 'use-http';
+import useHackerNewsStory from '../hooks/use-hacker-news-story';
 import useHackerNewsTopStories from '../hooks/use-hacker-news-top-stories';
 import HackerNewsService from '../services/hacker-news-service';
 import { shuffleArray } from '../utils/array-utils';
@@ -7,9 +9,36 @@ const MAX_TOP_STORIES_TO_RETRIEVE = 10;
 
 const HackerNews = () => {
     const hackerNewsService = new HackerNewsService();
-    const { topStoriesIds, loading } = useHackerNewsTopStories(hackerNewsService);
-    const tenRandomizedTopStories = !loading && shuffleArray(topStoriesIds).slice(0, MAX_TOP_STORIES_TO_RETRIEVE);
-    console.log(tenRandomizedTopStories)
+    const [ stories, setStories ] = React.useState<any>([]);
+    // const { topStoriesIds, loading } = useHackerNewsTopStories(hackerNewsService);
+    // const tenRandomizedTopStories: number[] = shuffleArray(topStoriesIds).slice(0, MAX_TOP_STORIES_TO_RETRIEVE);
+
+      React.useEffect(() => {
+        async function getTopStories() {
+            const url = "https://hacker-news.firebaseio.com/v0/topstories.json";
+            try {
+              const response = await fetch(url);
+              if (response.ok === false) {
+                throw new Error("Response Error:" + response.text);
+              }
+              const json = await response.json();
+
+              const promises = shuffleArray(json)
+                .slice(0, 10)
+                .map((id: number) =>
+                  fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
+                    response => response.json()
+                  )
+                );
+              const result = await Promise.all(promises);
+              return result;
+            } catch (err) {
+              console.error(err);
+            }
+          }
+          setStories(getTopStories().then(a => console.log(a)));
+          console.log(stories)
+      }, [])
     return (
         <section>123</section>
     )
