027d76481ba5fd2701f955d853298902f542649f
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-30 11:49:16 +0300

    Create hacker-news-stories.tsx

---
+++ src/components/hacker-news-stories.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import HackerNewsStory from './hacker-news-story';
+
+interface IHackerNewsStory {
+    by: string,
+    descendants: number,
+    id: number,
+    kids: Array<number>,
+    score: number,
+    time: number,
+    title: string,
+    type: string,
+    url: string,
+}
+
+const HackerNewsStories: React.FC<{ stories: Array<IHackerNewsStory> }> = ({ stories }) => (
+    <section className="hacker-news-stories">
+        <ul className="hacker-news-stories-list">
+            {
+                stories.map((story: IHackerNewsStory) => (
+                    <li key={story.id} className="hacker-news-stories-list-item">
+                        {/* <HackerNewsStory {...stories} /> */}
+                    </li>)
+                )
+            }
+        </ul>
+    </section>
+);
+
+export default HackerNewsStories;
