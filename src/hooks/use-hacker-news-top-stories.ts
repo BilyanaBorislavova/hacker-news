0b791cf79e961f048389ad372b4d6caf9664ff36
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-30 12:00:50 +0300

    Update use-hacker-news-top-stories.ts

--- src/hooks/use-hacker-news-top-stories.ts
+++ src/hooks/use-hacker-news-top-stories.ts
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import { useFetch } from 'use-http';
 import HackerNewsService from '../services/hacker-news-service';
 
-const useHackerNewsTopStories = (hackerNewsApi: HackerNewsService) => {
+const useHackerNewsTopStories = (service: HackerNewsService) => {
     const [ topStoriesIds, setTopStoriesIds ] = useState<Array<number>>([]);
-    const endpoint = hackerNewsApi.getTopStoriesIds();
+    const endpoint = service.getTopStoriesIds();
 
     const { data, loading } = useFetch(endpoint, []);
     
