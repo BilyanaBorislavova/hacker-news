b082d992be7dd68200962897707a58ee43848e33
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-30 13:44:01 +0300

    Create animated-hacker-icon.tsx

---
+++ src/components/animated/animated-hacker-icon.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { HackerIcon } from '../icons/icons';
+import AnimatedPulseIcon from './animated-pulse-icon';
+
+const AnimatedHackerIcon = () => (
+    <AnimatedPulseIcon>
+        <HackerIcon />
+    </AnimatedPulseIcon>
+);
+
+export default AnimatedHackerIcon;
